eb79b85343722830bf675e273120a6c123d6b797
yui62yui <you@example.com>
2023-07-07 13:51:25 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { nanoid } from "nanoid";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    {id: nanoid(), content: "리액트 공부하기"},
+    {id: nanoid(), content: "아침 운동하기"},
+  ]);
+
+  const [newTodo, setNewTodo] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input
+        value={newTodo}
+        onChange={(e)=>{
+          setNewTodo(e.target.value)
+        }}
+      />
+      <button onClick={()=>{
+        const newList = [...todoList, {id: nanoid(), content: newTodo}]
+        setTodoList(newList)
+        setNewTodo("")
+      }}>등록</button>
+
+      {/* nanoid 적용한 걸로 다시 고치기 */}
+
+      {todoList.map((todo)=>{
+        return (
+          <>
+            <br/>
+            <span>{todo.content}</span>
+            <button onClick={()=>{
+              const newArr = todoList.filter((a) => {
+                return a.id !== todo.id;
+              })
+              setTodoList(newArr)
+            }}>
+              삭제
+            </button>
+          </>
+        )
+      })}
+
+    </>
+ )
 }
 
 export default App;
---
+++ src/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div>
+      헉 모달이당
+    </div>
+  )
+}
+
+export default Modal

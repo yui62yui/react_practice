8e771f35978804890123618bd2004393c3a849f9
yui62yui <you@example.com>
2023-07-07 20:17:09 +0900

    1번 문제 해결 답안

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { nanoid } from "nanoid";
 
 function App() {
   const [todoList, setTodoList] = useState([
-    {id: nanoid(), content: "리액트 공부하기"},
-    {id: nanoid(), content: "아침 운동하기"},
+    {id: nanoid(), content: "리액트 공부하기", liked: 0 },
+    {id: nanoid(), content: "아침 운동하기", liked: 0},
   ]);
 
-  const [newTodo, setNewTodo] = useState("")
+  const [newTodo, setNewTodo] = useState("");
 
   return (
     <>
@@ -19,16 +19,14 @@ function App() {
         }}
       />
       <button onClick={()=>{
-        const newList = [...todoList, {id: nanoid(), content: newTodo}]
+        const newList = [...todoList, {id: nanoid(), content: newTodo, liked: 0}]
         setTodoList(newList)
         setNewTodo("")
       }}>등록</button>
 
-      {/* nanoid 적용한 걸로 다시 고치기 */}
-
       {todoList.map((todo)=>{
         return (
-          <>
+          <div key={todo.id}>
             <br/>
             <span>{todo.content}</span>
             <button onClick={()=>{
@@ -39,10 +37,32 @@ function App() {
             }}>
               삭제
             </button>
-          </>
+            <button onClick={() => {
+              // const newLiked = todo.liked + 1;
+              // // setTodoList([...todoList])
+              // // 1. setTodoList(todoList)는 안 되는데 왜지요?... 이때는 굳이 안 풀어써도 todoList가 바껴서 적용돼야 되지 않나요?...
+              // setTodoList({...todoList, like: newLiked})
+
+              // 1. todo 중 todo id와 같은 배열만 꺼낸다
+              // 2. todo.liked를 +1 늘려 준다
+              // 3. settodolist를 할때 todolist의 배열을 풀고, liked(key)를 수정해 준다.
+
+              const newArr = todoList.map((item)=>{
+                if (item.id === todo.id) {
+                  return {
+                    ...item,
+                    liked: item.liked +1
+                  }
+                }
+                return item;
+              });
+              setTodoList(newArr)
+            }}>
+                ♥ {todo.liked}
+            </button>
+          </div>
         )
       })}
-
     </>
  )
 }

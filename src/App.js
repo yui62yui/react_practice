744c0968f34fbe6c664ad9966bc269833adefaae
yui62yui <you@example.com>
2023-07-07 21:01:21 +0900

    SelectedList.jsx 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import SelectList from './component/SelectList';
 import TodoList from './component/TodoList';
 
 function App() {
   return (
     <>
       <TodoList />
+      <hr/>
+      <SelectList />
+      <hr/>
     </>
   )
 }
